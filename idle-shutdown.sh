e527c979f290ba6221a8def8a25ea553e7a2701b
EC2 Default User <ec2-user@ip-172-16-163-8.ec2.internal>
2024-09-12 16:47:11 +0000

    Updated SageMaker tutorial.

---
+++ idle-shutdown.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+# OVERVIEW
+# This script stops a SageMaker notebook once it's idle for more than 1 hour (default time)
+# You can change the idle time for stop using the environment variable below.
+# If you want the notebook the stop only if no browsers are open, remove the --ignore-connections flag
+#
+# Note that this script will fail if either condition is not met
+#   1. Ensure the Notebook Instance has internet connectivity to fetch the example config
+#   2. Ensure the Notebook Instance execution role permissions to SageMaker:StopNotebookInstance to stop the notebook
+#       and SageMaker:DescribeNotebookInstance to describe the notebook.
+
+# PARAMETERS
+IDLE_TIME=3600
+
+echo "Fetching the autostop script"
+wget https://raw.githubusercontent.com/aws-samples/amazon-sagemaker-notebook-instance-lifecycle-config-samples/master/scripts/auto-stop-idle/autostop.py
+
+echo "Detecting Python install with boto3 install"
+
+# Find which install has boto3 and use that to run the cron command. So will use default when available
+# Redirect stderr as it is unneeded
+CONDA_PYTHON_DIR=$(source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv && which python)
+if $CONDA_PYTHON_DIR -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR=$CONDA_PYTHON_DIR
+elif /usr/bin/python -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR='/usr/bin/python'
+else
+    # If no boto3 just quit because the script won't work
+    echo "No boto3 found in Python or Python3. Exiting..."
+    exit 1
+fi
+
+echo "Found boto3 at $PYTHON_DIR"
+echo "Starting the SageMaker autostop script in cron"
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * $PYTHON_DIR $PWD/autostop.py --time $IDLE_TIME --ignore-connections >> /var/log/jupyter.log") | crontab -
